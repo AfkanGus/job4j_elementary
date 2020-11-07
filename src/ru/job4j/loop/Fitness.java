f48687de4326eb79dbcb66520294d2e49588cce0
af.kan@bk.ru <Afkan1987>
2020-11-07 17:15:16 +0500

    5.4. Протеиновая диета [#393210]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,25 +3,26 @@ package ru.job4j.loop;
 import ru.job4j.calculator.Fit;
 
 public class Fitness {
-    public static int calc(int ivan, int nik){
+    public static int calc(int ivan, int nik) {
 
         //Количество месяцев по умолчанию
         int month = 0;
         while (ivan < nik) {
             //Если тяга ивана меньше, то количествоо месцев увеличивается
-            month +=1;
+            month += 1;
             // Иван увеличивает тягу в 3 раза
             ivan *= 3;
             // Ник в два раза
-            nik *=2;
+            nik *= 2;
             System.out.println(ivan);
+            //Выйти из цикла если ivan > nik
+            if (ivan > nik) {
+                break;
             }
-
-        //Выйти из цикла если ivan > nik
-         while (ivan > nik) break;
+        }
 
         return month;
     }
-    }
+}
 
 
