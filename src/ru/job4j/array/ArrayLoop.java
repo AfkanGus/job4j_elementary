2729d322de7be815e32f05657366d1c3d645b581
af.kan@bk.ru <Afkan1987>
2020-11-10 23:57:53 +0500

    6.0.3. Массивы и цикл for. [#393135]

---
+++ src/ru/job4j/array/ArrayLoop.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class ArrayLoop {
+    public static void main(String[] args) {
+        // Объявляем целочисленный массив из 4 эл.
+        int[] numbers = new int[4];
+
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index * 2 + 3;
+            //Вывод на консоль
+            System.out.println(numbers[index]);
+        }
+    }
+}
