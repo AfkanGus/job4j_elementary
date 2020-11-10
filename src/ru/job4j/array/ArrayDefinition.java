d83c4ea3938ee3cf02cfc0a3eafb2a92bac012de
afkan_guseynov <af.kan@bk.ru>
2020-11-10 16:23:29 +0500

    6.0.1.1. Размер массива [#393145]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,12 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         //Массив на 10 элементов типа short с именем ages.
         short[] ages = new short[10];
+        System.out.println(" Размер масисва ages  равен " + ages.length);
         //Массив на 100500 элементов типа String c именем surnames.
         String[] surnames = new String[100500];
+        System.out.println(" Размер масисва surname  равен " + surnames.length);
         //Массив на 40 элементов типа float с именем prices.
         float[] prices = new float[40];
+        System.out.println(" Размер масисва prise  равен " + prices.length);
     }
 }
