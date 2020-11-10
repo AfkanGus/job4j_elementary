41bb654e9191c02d74938d4568f8bd7e36c3628b
Afkan <57961759+AfkanGus@users.noreply.github.com>
2020-11-10 22:51:28 +0500

    Update MortgageTest.java

--- test/ru/job4j/loop/MortgageTest.java
+++ test/ru/job4j/loop/MortgageTest.java
@@ -15,6 +15,6 @@ public class MortgageTest {
     @Test
     public void when2Year() {
         int year = Mortgage.year(100,120,5);
-        assertThat(year,  is(2));
+        assertThat(year,  is(1));
     }
 }
