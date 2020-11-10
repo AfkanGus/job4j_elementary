ead0ce783fe00a256502aef8d93f64ca38b14eca
af.kan@bk.ru <Afkan1987>
2020-11-10 23:39:51 +0500

    6.0.2. Заполнение массива. [#393134]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,13 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+<<<<<<< HEAD
     public static void main(String[] args) {
+=======
+
+    public static void main(String[] args) {
+
+>>>>>>> 1bb2d2a (6.0.2. Заполнение массива. [#393134])
         //Массив на 10 элементов типа short с именем ages.
         short[] ages = new short[10];
         System.out.println(" Размер масисва ages  равен " + ages.length);
@@ -11,5 +17,19 @@ public class ArrayDefinition {
         //Массив на 40 элементов типа float с именем prices.
         float[] prices = new float[40];
         System.out.println(" Размер масисва prise  равен " + prices.length);
+<<<<<<< HEAD
+=======
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Vladimir Putin";
+        names[2] = "Lev Tolstoy";
+        names[3] = "Ivan Bunin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+>>>>>>> 1bb2d2a (6.0.2. Заполнение массива. [#393134])
     }
 }
