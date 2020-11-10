81998b4947cf108e59e344794cbddbce1f86b019
af.kan@bk.ru <Afkan1987>
2020-11-11 00:40:12 +0500

    6.0.3.1. Массивы и цикл for-earch [#393144]

--- src/ru/job4j/array/ArrayLoopForEarch.java
+++ src/ru/job4j/array/ArrayLoopForEarch.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-public class ArrayLoop {
+public class ArrayLoopForEarch {
     public static void main(String[] args) {
         // Объявляем целочисленный массив из 4 эл.
         int[] numbers = new int[4];
@@ -8,10 +8,11 @@ public class ArrayLoop {
             numbers[index] = index * 2 + 3;
         }
 
-        for (int index = 0; index < numbers.length; index++) {
+        //Имя переменной ind выбрано произвольно
+        for (int ind : numbers) {
 
-            //Вывод на консоль
-            System.out.println(numbers[index]);
+            //Вывод на консоль, ind
+            System.out.println(ind);
         }
     }
 }
