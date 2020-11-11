02c45e4c44059287da8155543f06e192fff3efe3
af.kan@bk.ru <Afkan1987>
2020-11-12 01:40:15 +0500

    6.2.2. Переставить элементы массива [#393139]

--- src/ru/job4j/array/SwitchArray.java
+++ src/ru/job4j/array/SwitchArray.java
@@ -1,18 +1,27 @@
 package ru.job4j.array;
 
+/*начение ячеек source и dest нужно поменять местами.*/
 public class SwitchArray {
+    //Метод будет менять местами произвольные элементы.
+    public static int[] swap(int[] array, int source, int dest) {
+        //array[source] - значение ячейки с индексом source
+        int tmp = array[source];
+        //array[dest] -значение ячейки с индеком dest
+        array[source] = array[dest];
+        //значение ячейки с индеком dest передаются временной переменой tmp
+        array[dest] = tmp;
+        return array;
+    }
 
     @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static int[] swapBorder(int[] array) {
-        //Создаю temp для  временного хранения значений
-        //ячейки с индиксом 0.
+        /*Создаю temp для  временного хранения значений ячейки с индиксом 0.*/
         int temp = array[0];
-        //Записываем в ячейку с 0, значение последнего
-        // элемента в массиве в индексом ячейки 5
-        array[0] = array[5];
-        //Записываем в ячейку с индексом 5
-        //временную переменную temp
-        array[5] = temp;
+        /* Записываем в ячейку с 0, значение элемента array.length - 1
+         * array.length - 1 - отнимаем 1, что бы предотвратит ошибку выхода за
+         * пределы массива на последней итерации */
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
         return array;
     }
 
