2856b2c3490b4ab8b9ca2cf0ab546ec400951d79
af.kan@bk.ru <Afkan1987>
2020-11-12 00:05:35 +0500

    6.2.1. Выход за границу массива. [#393138]

---
+++ src/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class SwitchArray {
+
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+    public static int[] swapBorder(int[] array) {
+        //Создаю temp для  временного хранения значений
+        //ячейки с индиксом 0.
+        int temp = array[0];
+        //Записываем в ячейку с 0, значение последнего
+        // элемента в массиве в индексом ячейки 5
+        array[0] = array[5];
+        //Записываем в ячейку с индексом 5
+        //временную переменную temp
+        array[5] = temp;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        //Метод swapBorder - меняет местами нулевой элемент и последний элемент в массиве.
+        int[] rsl = swapBorder(nums);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+    }
+}
