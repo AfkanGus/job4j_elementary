26480f15f6b47a72e7d31379fc963fe1a0a992b2
Afkan <57961759+AfkanGus@users.noreply.github.com>
2020-11-13 13:34:43 +0500

    Update Check.java

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,8 +4,8 @@ public class Check {
     /*Метод должен проверить, что все элементы в массиве являются true или false.*/
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        /*Длина массива data.length - 1, потому что нумератция начинается с 0*/
-        for (int index = 0; index < data.length - 1; index++) {
+       
+        for (int index = 0; index < data.length; index++) {
             /* Если ячейка с index не равно ячейки с длиной массива то вернем false */
             if (data[index] != data[data.length - 1]) {
                 return false;
