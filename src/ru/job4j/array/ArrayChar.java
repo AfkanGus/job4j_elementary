7be153cd91cf1d9656497489fc34dab87224a757
af.kan@bk.ru <Afkan1987>
2020-11-14 01:17:12 +0500

    6.4. Слово начинается с ... [#393122]

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -1,12 +1,14 @@
 package ru.job4j.array;
 
 public class ArrayChar {
+    //Метод проверяет, что слово начинается c определенной последовательности.
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        // В цикле прогоняем все элементы до длины plef
+        // В цикле прогоняем все элементы до длины pref
         for (int index = 0; index < pref.length; index++) {
-            //В условии сравниваем index
+            //В условии  сравнить значения ячеек с одинаковым индексом
             if (word[index] != pref[index]) {
+               // Если элемент отличается, значит, word не начинается со слова pref.
                 return false;
             }
         }
