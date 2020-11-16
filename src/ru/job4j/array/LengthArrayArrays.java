42237475a8fe3d2c59cd99a28b740c6187132ef8
Afkan <57961759+AfkanGus@users.noreply.github.com>
2020-11-16 22:32:36 +0500

    Update LengthArrayArrays.java

--- src/ru/job4j/array/LengthArrayArrays.java
+++ src/ru/job4j/array/LengthArrayArrays.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class LengthArrayArrays {
     public static void main(String[] args) {
-        //Cоздаем довухмерный массив с четырмя вложенными массивами
+        //Cоздаем двухмерный массив с четырмя вложенными массивами
         //Каждый из чет-ех влож.мас. имеет по 1,2,3,4 элемента
         int[][] numbers = {{1}, {2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
         for (int i = 0; i < numbers.length; i++) {
