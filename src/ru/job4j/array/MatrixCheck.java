25a6e68f6667fcdaa6283a82e3199c29e1afa004
afkan_guseynov <af.kan@bk.ru>
2020-11-17 15:28:59 +0500

    6.7.1. Моно строка в матрице. [#393131]

---
+++ src/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+/*В этом задании нужно проверить то, что строка в двухмерном массиве целиком заполнена символом 'X'.*/
+public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        //Перебираем все индексы ячейки в массиве board
+        for (int index = 0; index < board.length; index++) {
+            /*Если индексы строки и колонки([row][index]) не равны 'X'*/
+            if (board[row][index] != 'X') {
+                /*то вернем false*/
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
