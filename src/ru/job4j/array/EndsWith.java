7efa93941364fedda9fc4976be874935c0d92724
afkan_guseynov <af.kan@bk.ru>
2020-11-25 18:52:34 +0500

    6.4.2. Слово заканчивается на ... [#393126]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -1,15 +1,14 @@
 package ru.job4j.array;
-
+/**
+ * Метод endsWith проверяет то, что слово заканчивается на определенную
+ * последовательность. Если последлений индекс массива  word c вычитом
+ * из него i обрашение кпредыдушему элементу, не равное  post,вернем false
+ */
 public class EndsWith {
-    /* метод endsWith проверяет то, что слово заканчивается
-    на определенную последовательность.*/
-    public static boolean endsWith(char[] word,char[] post) {
+    public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int i = 0; i < post.length; i++) {
-            /*Если последлений индекс массива  word c вычитом из него i(обрашение к
-             * предыдушему элементу, не равное  post*/
-            if(word[word.length -1 -i] != post[post.length -1 -i])
-                /*вернем false*/
+            if (word[word.length - 1 - i] != post[post.length - 1 - i])
                 return false;
         }
         return result;
