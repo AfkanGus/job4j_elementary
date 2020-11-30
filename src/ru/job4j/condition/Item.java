01a3be1dea5b292ccad0e78060102e9f350f4669
AfkanGus <af.kan@bk.ru>
2020-11-30 22:29:59 +0500

    2. Перегрузка конструктора [#393325]

---
+++ src/ru/job4j/condition/Item.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item() {
+
+    }
+
+    public Item(int id) {
+
+    }
+
+    public Item(int id, String name) {
+
+    }
+}
