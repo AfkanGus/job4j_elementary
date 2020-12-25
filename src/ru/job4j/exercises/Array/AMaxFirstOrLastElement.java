c4a8bc1545d2cea32c67945c3b45b245c2e22ee5
afkan_guseynov <af.kan@bk.ru>
2020-12-25 23:19:43 +0500

    7. Максимум из первого и последнего элемента массива

---
+++ src/ru/job4j/exercises/Array/AMaxFirstOrLastElement.java
@@ -0,0 +1,28 @@
+package ru.job4j.exercises.Array;
+
+public class AMaxFirstOrLastElement {
+    public static int getMaxValue(int[] array) {
+      /*  int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[0] < array[array.length - 1]) {
+                return array[array.length - 1];
+            } else {
+                return array[0];
+            }
+        }
+        return count;
+    }*/
+        int rsl = array[0] < array[array.length - 1] ? 0 : array.length;
+        return rsl;
+    }
+}
+/*
+* 7. Максимум из первого и последнего элемента массива
+В этом задании продолжим закреплять индексацию в массиве,
+*  а также то, что первый элемент имеет индекс 0, а последний
+*  имя_массива.length - 1.
+
+Вам необходимо реализовать метод, который принимает массив
+* целочисленных значений, сравнивает между собой первый и
+* последний элемент и возвращает максимальное из двух
+* значений.*/
