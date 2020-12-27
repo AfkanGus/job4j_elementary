93318afba30a99cccd1ee58ba0a3ba483dbd9f75
AfkanGus <af.kan@bk.ru>
2020-12-27 13:53:11 +0500

    20. Вывести четные в порядке убывания индексов

---
+++ src/ru/job4j/exercises/Array/EvenPrint.java
@@ -0,0 +1,19 @@
+package ru.job4j.exercises.Array;
+
+public class EvenPrint {
+    public static void print(int[] array) {
+        for (int i = array.length-1; i >=0 ; i--) {
+            if(array[i]%2==0){
+                System.out.println(array[i]);
+            }
+        }
+
+
+    }
+}
+/*20. Вывести четные в порядке убывания индексов
+Дан целочисленный массив размера N. Вывести все
+содержащиеся в данном массиве четные числа в порядке
+убывания их индексов. Для вывода использовать
+System.out.println()
+Например, для {1, 2, 3, 4} вывести 4, 2*/
