f287356f3a068c197f369cd32709dbe5945d4582
afkan_guseynov <af.kan@bk.ru>
2021-01-01 22:14:53 +0500

    23. Переставить две строки в матрице

---
+++ src/ru/job4j/exercises/Array/SwapRows.java
@@ -0,0 +1,15 @@
+package ru.job4j.exercises.Array;
+
+public class SwapRows {
+    public static void swap(int[][] data, int src, int dst) {
+        for (int i = 0; i < data[src].length; i++) {
+            int rsl = data[src][i];
+            data[src][i] = data[dst][i];
+            data[dst][i] = rsl;
+        }
+    }
+}
+/*
+* 23. Переставить две строки в матрице
+Вам нужно переставить две строки в матрице,
+*  с учетом, что длины строк одинаковые. src, dest это индексы строк*/
