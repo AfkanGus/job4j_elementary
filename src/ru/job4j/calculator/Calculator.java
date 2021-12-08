3a58ded75889d23f3334a8cb0e94f2192d4d42bc
AfkanGus <af.kan@bk.ru>
2021-12-09 04:17:29 +0500

    1.9. Статические и не статические методы [#504806]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,51 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    private static final int X = 5;
+
+    /**
+     * @param a - параметр статического метода, принимающий число int
+     * @return разницу
+     */
+    public static int minus(int a) {
+        return a - X;
+    }
+
+    /**
+     * @param d - параметр нестатического метода
+     * @return
+     */
+    public int divide(int d) {
+        return d / X;
+    }
+
+    /**
+     * @param y - параметр статического метода
+     * @return
+     */
+    public static int sum(int y) {
+        return X + y;
+    }
+
+    public int multiply(int a) {
+        return X * a;
+    }
+
+    public int sumAllOperations(int a) {
+        return sum(a) + multiply(a) + minus(a) + divide(a);
+    }
+
     public static void main(String[] args) {
-        //Переменные со значениями 2,6,4,5.
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        //Переменны для хранения результата арифмитический операций.
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        // Вывод на консоль переменных sixDivTwo, fiveMinusTwo, fourTimeTwo.
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        Calculator calculator = new Calculator();
+        int minusRsl = calculator.multiply(10);
+        int divideRls = calculator.divide(25);
+        int sumRsl = Calculator.sum(5);
+        int multiRsl = sum(5);
+        int sumAllOperations = calculator.sumAllOperations(3);
+        System.out.println("sum = " + sumRsl);
+        System.out.println("minus = " + minusRsl);
+        System.out.println("multiply = " + multiRsl);
+        System.out.println("divide = " + divideRls);
+        System.out.println("all operations = " + sumAllOperations);
     }
 }
