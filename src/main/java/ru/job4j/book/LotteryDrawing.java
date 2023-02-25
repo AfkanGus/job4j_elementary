8a3db4a84e1a92456805db8b005c64a25d3e7a0b
afkan_guseynov <af.kan@bk.ru>
2023-02-25 21:15:57 +0400

    12. Вывод Ping Pong на консоль

--- src/main/java/ru/job4j/book/LotteryDrawing.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.book;
-
-public class LotteryDrawing {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/first/Task12Test.java
@@ -0,0 +1,23 @@
+package first;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Task12Test {
+    @Test
+    public void checkOutHelloJob4j() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Task12.main(null);
+        String expected = new StringJoiner(
+                System.lineSeparator(), "", System.lineSeparator()
+        ).add("Ping").add("Pong").toString();
+        assertThat(out.toString(), is(expected));
+    }
+}
