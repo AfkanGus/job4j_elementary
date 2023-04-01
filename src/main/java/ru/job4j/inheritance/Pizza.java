b4eb618b435d3e1530fb8af85bbadc35f667c9e1
afkan_guseynov <af.kan@bk.ru>
2023-04-02 00:38:43 +0400

    6. Вызов переопределенного метода родител

---
+++ src/main/java/ru/job4j/inheritance/Pizza.java
@@ -0,0 +1,10 @@
+package ru.job4j.inheritance;
+
+/**
+ * Вызов переопределенного метода родителя.
+ */
+public class Pizza {
+    public String name() {
+        return "Just tasty pizza";
+    }
+}
