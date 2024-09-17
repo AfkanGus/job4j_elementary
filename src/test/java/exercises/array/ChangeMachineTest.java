81ccbfa40fb1894e177c77378be41d511b014bf6
AfkanGus <af.kan@bk.ru>
2024-09-18 01:35:49 +0500

    Автомат размена купюр

---
+++ src/test/java/exercises/array/ChangeMachineTest.java
@@ -0,0 +1,58 @@
+package exercises.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * Автомат размена купюр.
+ */
+class ChangeMachineTest {
+
+    @Test
+    public void testGetChangeVariantsFor10() {
+        ChangeMachine changeMachine = new ChangeMachine();
+        int[][] expected = {
+                {1, 0, 0, 0},
+                {0, 2, 0, 0},
+                {0, 1, 2, 1},
+                {0, 0, 5, 0}
+        };
+
+        List<int[]> result = changeMachine.getChangeVariants(10);
+        Assertions.assertArrayEquals(expected, result.toArray());
+    }
+
+    @Test
+    public void testGetChangeVariantsFor5() {
+        ChangeMachine changeMachine = new ChangeMachine();
+        int[][] expected = {
+                {0, 1, 0, 0},
+                {0, 0, 2, 1}
+        };
+
+        List<int[]> result = changeMachine.getChangeVariants(5);
+        Assertions.assertArrayEquals(expected, result.toArray());
+    }
+
+    @Test
+    public void testGetChangeVariantsFor1() {
+        ChangeMachine changeMachine = new ChangeMachine();
+        int[][] expected = {
+                {0, 0, 0, 1}
+        };
+
+        List<int[]> result = changeMachine.getChangeVariants(1);
+        Assertions.assertArrayEquals(expected, result.toArray());
+    }
+
+    @Test
+    public void testGetChangeVariantsForInvalidAmount() {
+        ChangeMachine changeMachine = new ChangeMachine();
+        int[][] expected = {};
+
+        List<int[]> result = changeMachine.getChangeVariants(0); /*Неправильная сумма*/
+        Assertions.assertArrayEquals(expected, result.toArray());
+    }
+}
